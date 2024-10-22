dadcb5f9932bf87fed30f8f7b67de5905c921ec5
Benittostephen <benittostephen2000@gmail.com>
2024-10-22 17:10:03 +0530

    added otp screen

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -4,11 +4,38 @@ import 'package:nego_tube_login/pages/otp_verification_screen.dart';
 import 'package:nego_tube_login/widgets/my_textfield.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final emailController = TextEditingController();
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    // This is the email validator
+    String? _validateEmail(String? value) {
+      if (value == null || value.isEmpty) {
+        return 'Please enter your email';
+      }
+      final emailRegex =
+          RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+      if (!emailRegex.hasMatch(value)) {
+        return 'Please enter a valid email';
+      }
+      return null;
+    }
+
+    // This function can be called when submitting the form
+    void _submitForm(BuildContext context) {
+      if (_formKey.currentState!.validate()) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => OtpVerificationScreen(
+                    email: emailController.text.toLowerCase()))).then((_) {
+          emailController.clear();
+        });
+      }
+    }
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -17,97 +44,107 @@ class LoginScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 150.0),
-                const Text(
-                  'Get started',
-                  textHeightBehavior: TextHeightBehavior(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 35, fontWeight: FontWeight.bold, height: 0.8),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'Login to continue',
-                  style: TextStyle(fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(height: 70.0),
-                MyTextfield(controller: emailController, lableText: 'Email'),
-                const SizedBox(height: 10.0),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => OtpVerificationScreen()));
-                  },
-                  borderRadius: BorderRadius.circular(50),
-                  child: Ink(
-                    height: 45,
-                    decoration: BoxDecoration(
-                        color: primaryColor,
-                        borderRadius: BorderRadius.circular(50.0)),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Continue',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(width: 10),
-                        Icon(Icons.arrow_forward_ios,
-                            color: Colors.white, size: 18)
-                      ],
-                    ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // const SizedBox(height: 150.0),
+                  const SizedBox(height: 70.0),
+                  Image.asset('assets/icons/heart.png', scale: 5),
+                  const Text(
+                    'Get started',
+                    textHeightBehavior: TextHeightBehavior(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 35, fontWeight: FontWeight.bold, height: 0.8),
                   ),
-                ),
-                const SizedBox(height: 40.0),
-                const Row(
-                  children: [
-                    Expanded(child: Divider()),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or',
-                        style: TextStyle(fontSize: 15.0),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Login to continue',
+                    style: TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 70.0),
+                  MyTextfield(
+                      controller: emailController,
+                      hintText: 'Email',
+                      validator: _validateEmail),
+                  const SizedBox(height: 10.0),
+                  InkWell(
+                    onTap: () {
+                      _submitForm(context);
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => OtpVerificationScreen(
+                      //             email: emailController.text)));
+                    },
+                    borderRadius: BorderRadius.circular(50),
+                    child: Ink(
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.circular(50.0)),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Continue',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(Icons.arrow_forward_ios,
+                              color: Colors.white, size: 18)
+                        ],
                       ),
                     ),
-                    Expanded(child: Divider())
-                  ],
-                ),
-                const SizedBox(height: 40.0),
-                myButton('Continue with google', 'assets/icons/google.webp'),
-                // const SizedBox(height: 10.0),
-                // myButton('Continue With Facebook', 'assets/icons/facebook.png'),
-                const SizedBox(height: 10.0),
+                  ),
+                  const SizedBox(height: 40.0),
+                  const Row(
+                    children: [
+                      Expanded(child: Divider()),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or',
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                      Expanded(child: Divider())
+                    ],
+                  ),
+                  const SizedBox(height: 40.0),
+                  myButton('Continue with google', 'assets/icons/google.webp'),
+                  // const SizedBox(height: 10.0),
+                  // myButton('Continue With Facebook', 'assets/icons/facebook.png'),
+                  const SizedBox(height: 10.0),
 
-                // const Spacer(),
-                const SizedBox(height: 70),
-                const Text('by continuing, you agree to our'),
-                const SizedBox(height: 8.0),
+                  // const Spacer(),
+                  const SizedBox(height: 70),
+                  const Text('by continuing, you agree to our'),
+                  const SizedBox(height: 8.0),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    textButton('term of service', () {}),
-                    const SizedBox(width: 15.0),
-                    textButton('privacy policy', () {}),
-                    const SizedBox(width: 15.0),
-                    textButton('content policies', () {}),
-                  ],
-                ),
-                const SizedBox(height: 40.0),
-                const Text(
-                  'Powered By Heartinz \n©2020 Heartinz Technologies Pvt Ltd',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 10),
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      textButton('term of service', () {}),
+                      const SizedBox(width: 15.0),
+                      textButton('privacy policy', () {}),
+                      const SizedBox(width: 15.0),
+                      textButton('content policies', () {}),
+                    ],
+                  ),
+                  const SizedBox(height: 40.0),
+                  const Text(
+                    'Powered By Heartinz \n©2020 Heartinz Technologies Pvt Ltd',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 10),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/my_textfield.dart
+++ lib/widgets/my_textfield.dart
@@ -1,82 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:nego_tube_login/constants/const.dart';
+
+import '../constants/const.dart';
 
 class MyTextfield extends StatelessWidget {
   final TextEditingController controller;
-  final String lableText;
+  final String hintText;
+  final String? Function(String?)? validator;
 
   const MyTextfield(
-      {super.key, required this.controller, required this.lableText});
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.validator});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      keyboardType: TextInputType.emailAddress,
+      // validator: validator,
       controller: controller,
+      keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-          hintText: lableText,
-          hintStyle: TextStyle(color: primaryColor_300),
-          filled: true,
-          fillColor: primaryColor_50,
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: primaryColor_50, width: 1),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: primaryColor, width: 1),
-          ),
-          // border: OutlineInputBorder(
-          //   borderRadius: BorderRadius.circular(10),
-          //   //borderSide: BorderSide.none,
-          // ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          //labelText: lableText,
-          labelStyle: TextStyle(
-            color: Color(0xFFF454545),
-          )),
-      //validator: validator,
+        prefixIconConstraints:
+            const BoxConstraints(minWidth: 40, minHeight: 20),
+        filled: true,
+        fillColor: primaryColor_50,
+        hintText: hintText,
+        hintStyle: TextStyle(color: primaryColor_300, fontSize: 14),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: primaryColor_50),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide(color: primaryColor),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      ),
+      // autovalidateMode: AutovalidateMode.onUserInteraction,
     );
   }
 }
-
-// TextFormField _mytextfield(bool obscureText, TextEditingController controller,
-//     String lableText, String? Function(String?) validator, prefixicon) {
-//   return TextFormField(
-//     controller: controller,
-//     obscureText: obscureText,
-//     decoration: InputDecoration(
-//         filled: true,
-//         fillColor: Color(0xFFFf5f5f5),
-//         suffixIcon: passwordController.text.isNotEmpty
-//             ? (prefixicon)
-//             ? IconButton(
-//             icon: Icon(
-//               size: 18,
-//               color: Colors.grey,
-//               _obscureText ? Icons.visibility_off : Icons.visibility,
-//             ),
-//             onPressed: _togglePasswordVisibility)
-//             : null
-//             : null,
-//         enabledBorder: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(5),
-//           borderSide: BorderSide(color: Color(0xFFFC6C6C6), width: 1),
-//         ),
-//         focusedBorder: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(5),
-//           borderSide: BorderSide(color: widget.appColor, width: 1),
-//         ),
-//         border: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(5.0),
-//           //borderSide: BorderSide.none,
-//         ),
-//         contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-//         labelText: lableText,
-//         labelStyle: TextStyle(
-//           color: Color(0xFFF454545),
-//         )),
-//     // autovalidateMode: AutovalidateMode.onUserInteraction,
-//     validator: validator,
-//   );
-// }
